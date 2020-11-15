d64c10f1175fe037a36f62971ec07d142f1b7bc9
McCaffeteria <McCaffeteria@gmail.com>
2020-11-15 17:36:03 -0500

    arbitrary change to refresh private/public status ugh

--- In One Weekend/Basic Image Creation/Basic Image Creation.cpp
+++ In One Weekend/Basic Image Creation/Basic Image Creation.cpp
@@ -9,7 +9,8 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush; //std::cerr wries to the error output stream, instead of the standard output stream: std::cout
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        //std::cerr wries to the error output stream, instead of the standard output stream: std::cout or standard intput stream std::cin
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
